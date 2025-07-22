afb699e9d45a9e2743fcfc1af0ffb463b369de9e
Volodymyr Baranchuk <crackhead199701@gmail.com>
2025-07-22 14:55:39 +0300

    remove remotePosts, add more local content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,28 +1,14 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const localPosts = defineCollection({
+const posts = defineCollection({
   loader: glob({ pattern: "**/*.json", base: "./src/posts" }),
   schema: z.object({
     title: z.string(),
     body: z.string(),
+    description: z.string(),
+    image: z.string().optional(),
   }),
 });
 
-const remotePosts = defineCollection({
-  loader: async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?_start=1&_limit=5"
-    );
-    const data = await response.json();
-
-    return data.map((post: any) => ({ ...post, id: post.id.toString() }));
-  },
-  schema: z.object({
-    id: z.string(),
-    title: z.string(),
-    body: z.string(),
-  }),
-});
-
-export const collections = { localPosts, remotePosts };
+export const collections = { posts };
